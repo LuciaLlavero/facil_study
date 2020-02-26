b200d2c3adcc15a9217a49f048d49ea65c0f70b5
Lucía Llavero <48181651+LuciaLlavero@users.noreply.github.com>
2020-02-26 14:20:43 +0100

    summarize button active
    summarize button functionality added

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
-from datetime import datetime
-from pytz import timezone
-import getpass
 from app.forms import SummaryForm
 
-@app.route('/')
-@app.route('/index')
-def index() :
-	return render_template('index.html', user=getpass.getuser(), local_datetime=datetime.now(timezone('UTC')), form=SummaryForm())
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
+def index() : 
+	form = SummaryForm()
+	if form.validate_on_submit() :
+		return redirect(url_for('summary'))
+	return render_template('index.html', form=form)
+
+@app.route('/summary')
+def summary() :
+	print('test')
+	return render_template('summary.html')	
